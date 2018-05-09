a28a97cb698f16e6ac87556a3e34ae5b238990f8
Becky Pollard <hello@beckypollard.com>
2018-05-09 15:28:57 -0400

    Got the API to send the url to the API and we got back some info! YEEE!

--- scripts/main.js
+++ scripts/main.js
@@ -3,19 +3,22 @@ const app = {};
 //██████████ SETUP API ██████████
 app.setup = function() {
     //IMAGE TO POST
-    const sourceImageUrl = $(".inputImage").value;
+    const sourceImageUrl = $(".inputImage").val();
     //API CALL
-    return $.ajax({
+    $.ajax({
         url: "https://eastus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=age,gender,smile,facialHair,glasses,emotion,hair,makeup",
         headers: {
-            "Content-Type": "application.json",
+            "Content-Type": "application/json",
             "Accept": "application/json",
             "Ocp-Apim-Subscription-Key": "d4200ef51ed143d29345415ba54ad725"
         },
         method: "POST",
         data: '{"url": ' + '"' + sourceImageUrl + '"}', //TODO: TEMPLATE LITERAL THIS
     });
+    console.log("API CALLED");
+    console.log(sourceImageUrl);
 }
+$(".inputSubmit").on("click", app.setup);
 //██████████ EMOJI DATA ██████████
 app.emojis = function() {
     const emoji = {
@@ -29,8 +32,9 @@ app.emojis = function() {
 //██████████ INIT SETUP ██████████
 app.init = function(){
     app.setup();
+    app.emojis();
 }
 //██████████ INITIALIZE ██████████
 $(function(){
   app.init();
-}); 
+});
